fb5473e6abed08cb9467c0098a631ad16f4165e4
Nicolas Boiteux <nicolas_boiteux@yahoo.fr>
2019-06-30 06:35:06 +0200

    military items

--- vitems/hpp/military.hpp
+++ vitems/hpp/military.hpp
@@ -4,7 +4,6 @@
         description = "A small amount of explosive that could allow you to spend a pleasant moment during this day. To use with precautions all the same";
         weight = 5;
         nbusage = 1;
-        code = "vitems\items\c4.sqf";
         picture = "pictures\c4.jpg";
     };
     class missionplan {
@@ -13,7 +12,6 @@
         description = "Secret Defense - This information should not be disclosed to the enemy - Bring in sector c124535 the contents of this briefcase. Ask for colonel Jackson Bro";
         weight = 0.05;
         nbusage = 1;
-        code = "vitems\items\missionpan.sqf";
         picture = "pictures\mission.jpg";
     };
     class briefcase {
@@ -22,7 +20,6 @@
         description = "a small black leather briefcase quite light. It is locked with an electronic code.It is impossible to open it without destroying its contents";
         weight = 5;
         nbusage = 1;
-        code = "vitems\items\briefcase.sqf";
         picture = "pictures\briefcase.jpg";
     };
     class gpstracker {
@@ -31,6 +28,5 @@
         description = "A gps tracker that could be useful to us. The source of energy is a mystery.";
         weight = 0.2;
         nbusage = -1;
-        code = "vitems\items\gpstracker.sqf";
         picture = "pictures\gpstracker.jpg";
     };
