6a140a65e7b3a963a5bb10c92fb7f95a262f3745
Nicolas Boiteux <nicolas_boiteux@yahoo.fr>
2019-06-30 06:37:14 +0200

    add stuff items

--- vitems/hpp/stuff.hpp
+++ vitems/hpp/stuff.hpp
@@ -4,7 +4,6 @@
         description = "A cloth bandage that will allow you to stop bleeding quickly";
         weight = 0.1;
         nbusage = 1;
-        code = "vitems\items\bandage.sqf";
         picture = "pictures\bandage.jpg";
     };
     class sparewheel {
@@ -13,7 +12,6 @@
         description = "A deflated spare wheel";
         weight = 10;
         nbusage = 1;
-        code = "vitems\items\sparewheel.sqf";
         picture = "pictures\wheel.jpg";
     }; 
     class lighter {
@@ -22,7 +20,6 @@
         description = "A lighter that will allow you to light campfires quickly. This object is not waterproof";
         weight = 0.1;
         nbusage = 5;
-        code = "vitems\items\lighter.sqf";
         picture = "pictures\lighter.jpg";
     }; 
     class wrench {
@@ -30,8 +27,7 @@
         title = "Adjustable wrench";
         description = "A wrench that allows you to make the most basic repairs";
         weight = 1;
-        nbusage = 10;
-        code = "vitems\items\wrench.sqf";
+        nbusage = -1;
         picture = "pictures\wrench.jpg";
     }; 
     class miakalifa {
@@ -40,7 +36,6 @@
         description = "A figure of mia kalifa completely naked. She had, in all appearances, forgotten the whole epilation.";
         weight = 0.5;
         nbusage = -1;
-        code = "vitems\items\mia.sqf";
         picture = "pictures\mia.jpg";
     };
     class antibiotic {
@@ -49,7 +44,6 @@
         description = "A box full of antibiotic in an acceptable state";
         weight = 0.1;
         nbusage = 1;
-        code = "vitems\items\antibiotic.sqf";
         picture = "pictures\antibiotic.jpg";
     };
     class axe {
@@ -58,7 +52,6 @@
         description = "A Greenland Condor axe 1060 High Carbon";
         weight = 1;
         nbusage = -1;
-        code = "vitems\items\axe.sqf";
         picture = "pictures\axe.jpg";
     };
     class plank {
@@ -67,7 +60,6 @@
         description = "A pine wood plank of 6 to 8 feet";
         weight = 1;
         nbusage = 1;
-        code = "vitems\items\plank.sqf";
         picture = "pictures\plank.jpg";
     };
     class cigarettespacket {
@@ -76,6 +68,13 @@
         description = "A packet of french cigarettes.";
         weight = 0.2;
         nbusage = 20;
-        code = "vitems\items\cigarettes.sqf";
         picture = "pictures\cigarettes.jpg";
     };
+    class masse {
+        type[] = {"stuff"};
+        title = "A mass";
+        description = "Mass with fiberglass handle";
+        weight = 4;
+        nbusage = -1;
+        picture = "pictures\masse.jpg";
+    };    
