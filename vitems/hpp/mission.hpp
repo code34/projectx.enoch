36d4517bb35ea2f2704337b7063207aff4a08574
Nicolas Boiteux <nicolas_boiteux@yahoo.fr>
2019-07-06 21:57:00 +0200

    add mission items

---
+++ vitems/hpp/mission.hpp
@@ -0,0 +1,16 @@
+	class choppermotor {
+        type[] = {"mission"};
+        title = "Chopper motor";
+        description = "The Lycoming 235 Series Four-Cylinder Engine is a Popular Choice for Homebuilt and Original Aircraft";
+        weight = 60;
+        nbusage = 1;
+        picture = "pictures\choppermotor.jpg";
+    };
+    class armyradio {
+        type[] = {"mission"};
+        title = "Army radio";
+        description = "The versatile radio enables users to send information up and down the chain of command as well as across the battlefield network backbone";
+        weight = 1;
+        nbusage = -1;
+        picture = "pictures\armyradio.jpg";    	
+    };
