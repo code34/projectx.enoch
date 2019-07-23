802e36b9009e6d58702474b8924c1fa2d00ce734
Nicolas Boiteux <nicolas_boiteux@yahoo.fr>
2019-07-23 21:50:01 +0200

    add reserved objetcs

---
+++ vitems/hpp/reserved.hpp
@@ -0,0 +1,9 @@
+     class bottlewaterunverified {
+        type[] = {"reserved"};
+        title = "An unfiltred water bottle";
+        description = "A Plastic mineral unfiltred and unpure water bottle with nasty odor.";
+        weight = 1;
+        nbusage = 1;
+        picture = "pictures\bottlewaterunverified.jpg";
+        requirement[] = {};
+    };
