6662260011b9604c5c8e20bd9d94b8ddad7aee82
Nicolas Boiteux <nicolas_boiteux@yahoo.fr>
2019-07-23 21:48:04 +0200

    add weapons/mags

--- vitems/hpp/military.hpp
+++ vitems/hpp/military.hpp
@@ -25,3 +25,21 @@
         picture = "pictures\gpstracker.jpg";
         requirement[] = {};
     };
+    class riflemx {
+        type[] = {"military"};
+        title = "Rifle MX";
+        description = "A rifle MX weapons.";
+        weight = 0.5;
+        nbusage = -1;
+        picture = "\A3\Weapons_F_Exp\Rifles\MX\Data\UI\icon_arifle_MX_khk_F_X_CA.paa";
+        requirement[] = {};
+    };
+    class riflemxmag {
+        type[] = {"military"};
+        title = "Rifle MX mag";
+        description = "A rifle MX mag.";
+        weight = 0.2;
+        nbusage = 1;
+        picture = "\a3\Weapons_F\MagazineProxies\data\UI\icon_30Rnd_65x39_caseless_khaki_mag_CA.paa";
+        requirement[] = {};
+    };
