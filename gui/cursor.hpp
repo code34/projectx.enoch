3a206aacf0367512796f6ebd084213cc5bfcf0a3
code34 <nicolas_boiteux@yahoo.fr>
2020-05-08 17:56:42 +0200

    add well

--- gui/cursor.hpp
+++ gui/cursor.hpp
@@ -1,5 +1,5 @@
 class cursor {
-    idd = 1000;
+    idd = 5000;
     name= "cursor";
     movingEnable = false;
     enableSimulation = true;
@@ -21,7 +21,7 @@ class cursor {
                     w = 4 * pixelGrid * pixelW;
                     h = 3.6 * pixelGrid * pixelH;
                     text = "paa\look.paa";
-                    tooltip = "inventory";
+                    tooltip = "";
                 };
             };
         };
@@ -29,6 +29,34 @@ class cursor {
     class controls {};
 };
 
-/*
-["cursor",1000,[[[["78 * pixelGrid * pixelW","37.8 * pixelGrid * pixelH","4 * pixelGrid * pixelW","3.6 * pixelGrid * pixelH"],"""png\look.png""","look","inventory","OOP_Picture",true,[],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1]]]]]
-*/
+
+class cursorpump {
+    idd = 5010;
+    name= "cursorpump";
+    movingEnable = false;
+    enableSimulation = true;
+    duration = 1000000;
+    onLoad = "";
+    onUnload = "";
+    class controlsBackground {
+        class OOP_MainLayer_100_100 : OOP_MainLayer {
+            idc = 100;
+            x = -38 * pixelGrid * pixelW;
+            y = -13.5 * pixelGrid * pixelH;
+            w = 160 * pixelGrid * pixelW;
+            h = 90 * pixelGrid * pixelH;
+            class controls{
+                class look_101: OOP_Picture {
+                    idc = 101;
+                    x = 78 * pixelGrid * pixelW;
+                    y = 37.8 * pixelGrid * pixelH;
+                    w = 6 * pixelGrid * pixelW;
+                    h = 6 * pixelGrid * pixelH;
+                    text = "paa\waterpump.paa";
+                    tooltip = "";
+                };
+            };
+        };
+    };
+    class controls {};
+};
--- gui/cursor.sqf
+++ gui/cursor.sqf
@@ -1,20 +1,32 @@
 		private _active = false;
+		private _pump = ["stonewell_01_f.p3d","concretewell_02_f.p3d"];
 		while { true } do {
-			_size = 0;
+			private _size = 0;
 			_canbeprint = false;
-			if ((typeof cursorObject) isKindOf "House_F") then {
-				_size = ((2 boundingBoxReal cursorObject) select 2) - 1;
-			} else {
-				_size = ((2 boundingBoxReal cursorObject) select 2) + 2;
+			switch (true) do { 
+				case (((getModelInfo cursorObject) select 0) in _pump) : {
+					_size = 3;
+				}; 
+				case ((typeof cursorObject) isKindOf "House_F") : {
+					_size = ((2 boundingBoxReal cursorObject) select 2) - 1;
+				}; 
+				default {
+					_size = ((2 boundingBoxReal cursorObject) select 2) + 2;
+				}; 
 			};
 			if (isnull (findDisplay 1000) and isnull (findDisplay 602)) then { _canbeprint = true;};
 			if ((cursorObject distance player < _size) and (!(cursorObject isKindOf "Man") or !(alive cursorObject)) and _canbeprint) then {
 				if!(_active) then {
-					1001 cutRsc ["cursor", "PLAIN"];
+					if!(((getModelInfo cursorObject) select 0) in _pump) then {
+						5000 cutRsc ["cursor", "PLAIN"];
+					} else {
+						5010 cutRsc ["cursorpump", "PLAIN"];
+					};
 					_active = true;
 				};
 			} else {
-				1001 cutText ["", "PLAIN"];
+				5000 cutText ["", "PLAIN"];
+				5010 cutText ["", "PLAIN"];
 				_active = false;
 			};
 			sleep 0.1;
