a1bd3309b8dce0ed1594394d87ede6555e0dc050
code34 <nicolas_boiteux@yahoo.fr>
2020-05-10 22:00:01 +0200

    typo

--- vitems/hpp/stuff.hpp
+++ vitems/hpp/stuff.hpp
@@ -1,153 +1,153 @@
-    class bandage {
-        type[] = {"stuff","man"};
-        title = "A cloth bandage";
-        description = "A cloth bandage that will allow you to stop bleeding quickly";
-        weight = 0.1;
-        nbusage = 1;
-        picture = "paa\bandage.paa";
-		requirement[] = {};        
-    };
-    class sparewheel {
-        type[] = {"stuff"};
-        title = "Spare wheel";
-        description = "A deflated spare wheel";
-        weight = 10;
-        nbusage = 1;
-        picture = "paa\wheel.paa";
-		requirement[] = {"wrench"};        
-    }; 
-    class lighter {
-        type[] = {"stuff","man"};
-        title = "A lighter";
-        description = "A lighter that will allow you to light campfires quickly. This object is not waterproof";
-        weight = 0.1;
-        nbusage = -1;
-        picture = "paa\lighter.paa";
-		requirement[] = {};        
-    }; 
-    class wrench {
-        type[] = {"stuff"};
-        title = "Adjustable wrench";
-        description = "A wrench that allows you to make the most basic repairs";
-        weight = 1;
-        nbusage = -1;
-        picture = "paa\wrench.paa";
-        requirement[] = {};
-    }; 
-    class miakalifa {
-        type[] = {"stuff"};
-        title = "A figurine of mia kalifa";
-        description = "A figure of mia kalifa completely naked. She had, in all appearances, forgotten the whole epilation.";
-        weight = 0.5;
-        nbusage = -1;
-        picture = "paa\mia.paa";
-        requirement[] = {};
-    };
-    class antibiotic {
-        type[] = {"stuff","man"};
-        title = "Antibiotic";
-        description = "A box full of antibiotic in an acceptable state";
-        weight = 0.1;
-        nbusage = 1;
-        picture = "paa\antibiotic.paa";
-        requirement[] = {};
-    };
-    class axe {
-        type[] = {"stuff"};
-        title = "Axe";
-        description = "A Greenland Condor axe 1060 High Carbon";
-        weight = 1;
-        nbusage = -1;
-        picture = "paa\axe.paa";
+	class bandage {
+		type[] = {"stuff","man"};
+		title = "A cloth bandage";
+		description = "A cloth bandage that will allow you to stop bleeding quickly";
+		weight = 0.1;
+		nbusage = 1;
+		picture = "paa\bandage.paa";
 		requirement[] = {};
-    };
-    class plank {
-        type[] = {"stuff"};
-        title = "Plank";
-        description = "A pine wood plank of 6 to 8 feet";
-        weight = 1;
-        nbusage = 1;
-        picture = "paa\plank.paa";
+	};
+	class sparewheel {
+		type[] = {"stuff"};
+		title = "Spare wheel";
+		description = "A deflated spare wheel";
+		weight = 10;
+		nbusage = 1;
+		picture = "paa\wheel.paa";
+		requirement[] = {"wrench"};
+	}; 
+	class lighter {
+		type[] = {"stuff","man"};
+		title = "A lighter";
+		description = "A lighter that will allow you to light campfires quickly. This object is not waterproof";
+		weight = 0.1;
+		nbusage = -1;
+		picture = "paa\lighter.paa";
 		requirement[] = {};
-    };
-    class cigarettespacket {
-        type[] = {"stuff"};
-        title = "Cigarettes packet";
-        description = "A packet of french cigarettes.";
-        weight = 0.2;
-        nbusage = 1;
-        picture = "paa\cigarettes.paa";
+	}; 
+	class wrench {
+		type[] = {"stuff"};
+		title = "Adjustable wrench";
+		description = "A wrench that allows you to make the most basic repairs";
+		weight = 1;
+		nbusage = -1;
+		picture = "paa\wrench.paa";
+		requirement[] = {};
+	}; 
+	class miakalifa {
+		type[] = {"stuff"};
+		title = "A figurine of mia kalifa";
+		description = "A figure of mia kalifa completely naked. She had, in all appearances, forgotten the whole epilation.";
+		weight = 0.5;
+		nbusage = -1;
+		picture = "paa\mia.paa";
+		requirement[] = {};
+	};
+	class antibiotic {
+		type[] = {"stuff","man"};
+		title = "Antibiotic";
+		description = "A box full of antibiotic in an acceptable state";
+		weight = 0.1;
+		nbusage = 1;
+		picture = "paa\antibiotic.paa";
+		requirement[] = {};
+	};
+	class axe {
+		type[] = {"stuff"};
+		title = "Axe";
+		description = "A Greenland Condor axe 1060 High Carbon";
+		weight = 1;
+		nbusage = -1;
+		picture = "paa\axe.paa";
+		requirement[] = {};
+	};
+	class plank {
+		type[] = {"stuff"};
+		title = "Plank";
+		description = "A pine wood plank of 6 to 8 feet";
+		weight = 1;
+		nbusage = 1;
+		picture = "paa\plank.paa";
+		requirement[] = {};
+	};
+	class cigarettespacket {
+		type[] = {"stuff"};
+		title = "Cigarettes packet";
+		description = "A packet of french cigarettes.";
+		weight = 0.2;
+		nbusage = 1;
+		picture = "paa\cigarettes.paa";
 		requirement[] = {"lighter"};
-    };
-    class mace {
-        type[] = {"stuff"};
-        title = "A mace";
-        description = "Mace with fiberglass handle";
-        weight = 4;
-        nbusage = -1;
-        picture = "paa\mace.paa";
+	};
+	class mace {
+		type[] = {"stuff"};
+		title = "A mace";
+		description = "Mace with fiberglass handle";
+		weight = 4;
+		nbusage = -1;
+		picture = "paa\mace.paa";
 		requirement[] = {};
-    };
-    class hose {
-        type[] = {"stuff"};
-        title = "An engine hose";
-        description = "Tube has been upgraded to meet changes in fuel characteristics and resists heat and oil.";
-        weight = 0.2;
-        nbusage = 1;
-        picture = "paa\hose.paa";
-        requirement[] = {};
-    };
-    class checkvalve {
-        type[] = {"stuff"};
-        title = "Check valve";
-        description = "Aluminum alloy check valve, one-way diesel fuel for cars, helicopters, motorcycles, fuel pumps.";
-        weight = 0.1;
-        nbusage = 1;
-        picture = "paa\checkvalve.paa";
-        requirement[] = {"hose"};
-    };
-    class screwdriver {
-        type[] = {"stuff"};
-        title = "Screw driver";
-        description = "The high quality bits have been designed for an improved, long-lasting life, while the built-in bolster allows for additional leverage and torque.";
-        weight = 0.2;
-        nbusage = -1;
-        picture = "paa\screwdriver.paa";
-        requirement[] = {};
-    };
-    class aquatablet {
-        type[] = {"stuff"};
-        title = "Potable aqua";
-        description = "Potable Aqua Water Purification Tablets.";
-        weight = 0.2;
-        nbusage = 1;
-        picture = "paa\aquatablet.paa";
-        requirement[] = {"bottlewaterunverified"};
-    };
-     class emptybottle {
-        type[] = {"stuff"};
-        title = "A plastic bottle";
-        description = "A Plastic empty bottle.";
-        weight = 0.1;
-        nbusage = 1;
-        picture = "paa\emptybottle.paa";
-        requirement[] = {};
-    };
-     class tinopener {
-        type[] = {"stuff"};
-        title = "Tin opener";
-        description = "Swedish tin opener.";
-        weight = 0.1;
-        nbusage = -1;
-        picture = "paa\tinopener.paa";
-        requirement[] = {};
-    };
-    class briefcase {
-        type[] = {"stuff"};
-        title = "A little briefcase";
-        description = "a small black leather briefcase quite light. It is locked with an electronic code.It is impossible to open it without destroying its contents";
-        weight = 5;
-        nbusage = 1;
-        picture = "paa\briefcase.paa";
-        requirement[] = {};
-    };
+	};
+	class hose {
+		type[] = {"stuff"};
+		title = "An engine hose";
+		description = "Tube has been upgraded to meet changes in fuel characteristics and resists heat and oil.";
+		weight = 0.2;
+		nbusage = 1;
+		picture = "paa\hose.paa";
+		requirement[] = {};
+	};
+	class checkvalve {
+		type[] = {"stuff"};
+		title = "Check valve";
+		description = "Aluminum alloy check valve, one-way diesel fuel for cars, helicopters, motorcycles, fuel pumps.";
+		weight = 0.1;
+		nbusage = 1;
+		picture = "paa\checkvalve.paa";
+		requirement[] = {"hose"};
+	};
+	class screwdriver {
+		type[] = {"stuff"};
+		title = "Screw driver";
+		description = "The high quality bits have been designed for an improved, long-lasting life, while the built-in bolster allows for additional leverage and torque.";
+		weight = 0.2;
+		nbusage = -1;
+		picture = "paa\screwdriver.paa";
+		requirement[] = {};
+	};
+	class aquatablet {
+		type[] = {"stuff"};
+		title = "Potable aqua";
+		description = "Potable Aqua Water Purification Tablets.";
+		weight = 0.2;
+		nbusage = 1;
+		picture = "paa\aquatablet.paa";
+		requirement[] = {"bottlewaterunverified"};
+	};
+	 class emptybottle {
+		type[] = {"stuff"};
+		title = "A plastic bottle";
+		description = "A Plastic empty bottle.";
+		weight = 0.1;
+		nbusage = 1;
+		picture = "paa\emptybottle.paa";
+		requirement[] = {};
+	};
+	 class tinopener {
+		type[] = {"stuff"};
+		title = "Tin opener";
+		description = "Swedish tin opener.";
+		weight = 0.1;
+		nbusage = -1;
+		picture = "paa\tinopener.paa";
+		requirement[] = {};
+	};
+	class briefcase {
+		type[] = {"stuff"};
+		title = "A little briefcase";
+		description = "a small black leather briefcase quite light. It is locked with an electronic code.It is impossible to open it without destroying its contents";
+		weight = 5;
+		nbusage = 1;
+		picture = "paa\briefcase.paa";
+		requirement[] = {};
+	};
