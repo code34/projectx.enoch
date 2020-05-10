42383d2944f6146fb362a66a3d5070787fb47d55
code34 <nicolas_boiteux@yahoo.fr>
2020-05-10 21:59:24 +0200

    typo

--- vitems/hpp/military.hpp
+++ vitems/hpp/military.hpp
@@ -1,36 +1,36 @@
-    class explosivec4 {
-        type[] = {"military"};
-        title = "Explosive C4";
-        description = "A small amount of explosive that could allow you to spend a pleasant moment during this day. To use with precautions all the same";
-        weight = 5;
-        nbusage = 1;
-        picture = "paa\c4.paa";
-		requirement[] = {};        
-    };
-    class gpstracker {
-        type[] = {"military"};
-        title = "GPS tracker";
-        description = "A gps tracker that could be useful to us. The source of energy is a mystery.";
-        weight = 0.2;
-        nbusage = 1;
-        picture = "paa\gpstracker.paa";
-        requirement[] = {};
-    };
-    class survivalration {
-        type[] = {"military"};
-        title = "Survival Ration";
-        description = "Ultimate Survival Technologies 5-Year Emergency Food Ration Bar.<br/><br/>Drink: <t color='#ff9d00'>+20</t> <br/>Food: <t color='#cc0000'>+20</t>";
-        weight = 0.2;
-        nbusage = 1;
-        picture = "paa\survivalration.paa";
-        requirement[] = {};
-    };
-    class medicalkit {
-        type[] = {"military"};
-        title = "Medical Kit";
-        description = "First aid kit.";
-        weight = 2;
-        nbusage = 1;
-        picture = "paa\medicalkit.paa";
-        requirement[] = {};
-    };
+	class explosivec4 {
+		type[] = {"military"};
+		title = "Explosive C4";
+		description = "A small amount of explosive that could allow you to spend a pleasant moment during this day. To use with precautions all the same";
+		weight = 5;
+		nbusage = 1;
+		picture = "paa\c4.paa";
+		requirement[] = {};
+	};
+	class gpstracker {
+		type[] = {"military"};
+		title = "GPS tracker";
+		description = "A gps tracker that could be useful to us. The source of energy is a mystery.";
+		weight = 0.2;
+		nbusage = 1;
+		picture = "paa\gpstracker.paa";
+		requirement[] = {};
+	};
+	class survivalration {
+		type[] = {"military"};
+		title = "Survival Ration";
+		description = "Ultimate Survival Technologies 5-Year Emergency Food Ration Bar.<br/><br/>Drink: <t color='#ff9d00'>+20</t> <br/>Food: <t color='#cc0000'>+20</t>";
+		weight = 0.2;
+		nbusage = 1;
+		picture = "paa\survivalration.paa";
+		requirement[] = {};
+	};
+	class medicalkit {
+		type[] = {"military"};
+		title = "Medical Kit";
+		description = "First aid kit.";
+		weight = 2;
+		nbusage = 1;
+		picture = "paa\medicalkit.paa";
+		requirement[] = {};
+	};
