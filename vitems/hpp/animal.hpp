5a57785a4f7d9b46a166189c471fca92608257a4
code34 <nicolas_boiteux@yahoo.fr>
2020-11-11 01:34:37 +0100

    add animals

---
+++ vitems/hpp/animal.hpp
@@ -0,0 +1,9 @@
+	class meat {
+		type[] = {"animal"};
+		title = "Meat";
+		description = "A nice animal meat.<br/><br/>Drink: <t color='#cc0000'>0</t> <br/>Food: <t color='#ff9d00'>+20</t>";
+		weight = 0.1;
+		nbusage = 1;
+		picture = "paa\zombiemeat.paa";
+		requirement[] = {};
+	};
---
+++ vitems/items/meat.sqf
@@ -0,0 +1,1 @@
+	[20,true] call vitems_eating;
